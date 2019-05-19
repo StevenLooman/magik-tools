8d215acfe37af8025036adda001e6231622926ea
Steven Looman <steven.looman@gmail.com>
2019-05-19 18:26:44 +0200

    Don't keep lists, use annotations/reflection

--- sonar-magik-plugin/src/main/java/org/stevenlooman/sw/sonar/MagikRuleRepository.java
+++ sonar-magik-plugin/src/main/java/org/stevenlooman/sw/sonar/MagikRuleRepository.java
@@ -1,14 +1,12 @@
 package org.stevenlooman.sw.sonar;
 
-import com.google.common.collect.Sets;
-
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonarsource.analyzer.commons.RuleMetadataLoader;
 import org.stevenlooman.sw.magik.CheckList;
+import org.stevenlooman.sw.magik.TemplatedCheck;
 import org.stevenlooman.sw.sonar.language.Magik;
 
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -17,12 +15,13 @@ public class MagikRuleRepository implements RulesDefinition {
   static final String REPOSITORY_NAME = "SonarAnalyzer";
   static final String RESOURCE_FOLDER = "org/stevenlooman/sw/sonar/l10n/magik/rules";
 
-  private static final Set<String> TEMPLATE_RULE_KEYS = Sets.newHashSet(
-      "CommentRegularExpression",
-      "LineLength",
-      "MethodComplexity",
-      "XPath"
-      );
+  private List<String> templatedRules() {
+    return getCheckClasses().stream()
+        .filter(klass -> klass.getAnnotation(TemplatedCheck.class) != null)
+        .map(klass -> klass.getAnnotation(org.sonar.check.Rule.class))
+        .map(rule -> ((org.sonar.check.Rule)rule).key())
+        .collect(Collectors.toList());
+  }
 
   @Override
   public void define(Context context) {
@@ -33,8 +32,9 @@ public class MagikRuleRepository implements RulesDefinition {
     RuleMetadataLoader loader = new RuleMetadataLoader(RESOURCE_FOLDER, CheckList.PROFILE_LOCATION);
     loader.addRulesByAnnotatedClass(repository, getCheckClasses());
 
+    List<String> templatedRules = templatedRules();
     repository.rules().stream()
-        .filter(rule -> TEMPLATE_RULE_KEYS.contains(rule.key()))
+        .filter(rule -> templatedRules.contains(rule.key()))
         .forEach(rule -> rule.setTemplate(true));
 
     repository.done();
