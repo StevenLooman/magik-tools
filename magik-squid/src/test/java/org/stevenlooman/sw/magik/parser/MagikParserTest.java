f390927f1e562b35a59338caf36b770b084bea5e
Steven Looman <steven.looman@gmail.com>
2019-07-08 22:07:26 +0200

    Add more tests to MagikParserTest

--- magik-squid/src/test/java/org/stevenlooman/sw/magik/parser/MagikParserTest.java
+++ magik-squid/src/test/java/org/stevenlooman/sw/magik/parser/MagikParserTest.java
@@ -38,4 +38,50 @@ public class MagikParserTest {
     assertThat(syntaxErrorNode.getToken().getColumn()).isEqualTo(1);
   }
 
+  @Test
+  public void testInvalidMagik2() {
+    MagikParser parser = new MagikParser(Charset.forName("UTF-8"));
+    String code =
+        "_method object.m1\n" +
+        "\t_block\n" +
+        "\t_endbloc\n" +
+        "_endmethod";
+    AstNode node = parser.parse(code);
+    assertThat(node.getChildren()).hasSize(1);
+    AstNode syntaxErrorNode = node.getChildren().get(0);
+    assertThat(syntaxErrorNode.getType()).isEqualTo(MagikGrammar.SYNTAX_ERROR);
+    assertThat(syntaxErrorNode.getToken().getLine()).isEqualTo(3);
+    assertThat(syntaxErrorNode.getToken().getColumn()).isEqualTo(2);
+  }
+
+  @Test
+  public void testInvalidMagik3() {
+    MagikParser parser = new MagikParser(Charset.forName("UTF-8"));
+    String code =
+        "_method object.m2\n" +
+        "\t_local a << {1 2}\n" +
+        "_endmethod";
+    AstNode node = parser.parse(code);
+    assertThat(node.getChildren()).hasSize(1);
+    AstNode syntaxErrorNode = node.getChildren().get(0);
+    assertThat(syntaxErrorNode.getType()).isEqualTo(MagikGrammar.SYNTAX_ERROR);
+    assertThat(syntaxErrorNode.getToken().getLine()).isEqualTo(2);
+    assertThat(syntaxErrorNode.getToken().getColumn()).isEqualTo(17);
+  }
+
+  @Test
+  public void testInvalidMagik4() {
+    MagikParser parser = new MagikParser(Charset.forName("UTF-8"));
+    String code =
+        "_method object.m3\n" +
+        "\t_local a << prc(:)\n" +
+        "_endmethod";
+    AstNode node = parser.parse(code);
+    assertThat(node.getChildren()).hasSize(1);
+    AstNode syntaxErrorNode = node.getChildren().get(0);
+    assertThat(syntaxErrorNode.getType()).isEqualTo(MagikGrammar.SYNTAX_ERROR);
+    assertThat(syntaxErrorNode.getToken().getLine()).isEqualTo(2);
+    assertThat(syntaxErrorNode.getToken().getColumn()).isEqualTo(18);
+  }
+
 }
