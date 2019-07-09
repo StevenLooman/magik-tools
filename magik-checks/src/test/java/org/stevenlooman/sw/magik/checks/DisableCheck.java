fc56b2a0ed7523a416bdef478d419bbb111d596e
Steven Looman <steven.looman@gmail.com>
2019-07-09 22:52:51 +0200

    Better handle multiple disabled checks

--- magik-checks/src/test/java/org/stevenlooman/sw/magik/checks/DisableCheck.java
+++ magik-checks/src/test/java/org/stevenlooman/sw/magik/checks/DisableCheck.java
@@ -21,7 +21,20 @@ public class DisableCheck extends MagikCheckTestBase {
   }
 
   @Test
-  public void testDoFail() {
+  public void testNoMLintDouble() {
+    MagikCheck check1 = new SizeZeroEmptyCheck();
+    MagikCheck check2 = new LhsRhsComparatorEqualCheck();
+
+    String code =
+        "a.size = 0 _andif a.size = 0  # mlint: disable=size-zero-empty, lhs-rhs-comparator-equal";
+    List<MagikIssue> issues1 = runCheck(code, check1);
+    assertThat(issues1).isEmpty();
+    List<MagikIssue> issues2 = runCheck(code, check2);
+    assertThat(issues2).isEmpty();
+  }
+
+  @Test
+  public void testYesMLint() {
     MagikCheck check = new SizeZeroEmptyCheck();
 
     String code =
