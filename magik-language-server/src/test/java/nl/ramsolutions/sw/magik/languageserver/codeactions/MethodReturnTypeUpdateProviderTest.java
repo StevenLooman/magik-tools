890d3de2b736b3fff8015efb17aa1e8915719423
Steven Looman <steven.looman@gmail.com>
2023-08-01 22:57:53 +0200

    Fix tests on Windows

--- magik-language-server/src/test/java/nl/ramsolutions/sw/magik/languageserver/codeactions/MethodReturnTypeUpdateProviderTest.java
+++ magik-language-server/src/test/java/nl/ramsolutions/sw/magik/languageserver/codeactions/MethodReturnTypeUpdateProviderTest.java
@@ -21,6 +21,8 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 class MethodReturnTypeUpdateProviderTest {
 
+    private static final String NEWLINE = System.lineSeparator();
+
     private List<CodeAction> getCodeActions(
             final String code,
             final Range range) {
@@ -52,7 +54,7 @@ class MethodReturnTypeUpdateProviderTest {
             final List<TextEdit> changes = textDocumentEdit.getEdits();
             assertThat(uri).isEqualTo("tests://unittest");
             assertThat(changes).hasSize(1);
-            assertThat(changes.get(0).getNewText()).isEqualTo("\t## @return {sw:integer} Description\n");
+            assertThat(changes.get(0).getNewText()).isEqualTo("\t## @return {sw:integer} Description" + NEWLINE);
         });
     }
 
