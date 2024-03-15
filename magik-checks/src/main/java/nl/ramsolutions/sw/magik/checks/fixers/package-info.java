c14389f6e3ac572c464a66ef8ca2fe9b099c8d92
Steven Looman <steven.looman@gmail.com>
2024-03-15 15:19:37 +0100

    Linting

--- magik-checks/src/main/java/nl/ramsolutions/sw/magik/checks/fixers/package-info.java
+++
@@ -1,4 +0,0 @@
-/** Defaults. */
-@edu.umd.cs.findbugs.annotations.DefaultAnnotationForParameters(
-    edu.umd.cs.findbugs.annotations.NonNull.class)
-package nl.ramsolutions.sw.magik.checks.fixers;
