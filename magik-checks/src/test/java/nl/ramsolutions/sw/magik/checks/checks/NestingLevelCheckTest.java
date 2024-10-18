0610c13c31d34e195523d593fa1a34cfe655f8ec
Sebastiaan Speck <shem.speck@gmail.com>
2024-10-18 22:43:54 +0200

    Flip to failing tests (for now)

--- magik-checks/src/test/java/nl/ramsolutions/sw/magik/checks/checks/NestingLevelCheckTest.java
+++ magik-checks/src/test/java/nl/ramsolutions/sw/magik/checks/checks/NestingLevelCheckTest.java
@@ -12,7 +12,7 @@ import org.junit.jupiter.api.Test;
 class NestingLevelCheckTest extends MagikCheckTestBase {
 
   @Test
-  void testTooComplex() {
+  void testExceedingMaximumNestingLevel() {
     final NestingLevelCheck check = new NestingLevelCheck();
 
     final String code =
@@ -36,11 +36,35 @@ class NestingLevelCheckTest extends MagikCheckTestBase {
         _endmethod
         """;
     final List<MagikIssue> issues = this.runCheck(code, check);
-    assertThat(issues).hasSize(2); // FIXME: Should be 1 since we didn't close the first `_if`-statement yet.
+    // Should be 1 since we did not close the first `_if`-statement yet.
+    assertThat(issues).hasSize(1);
   }
 
   @Test
-  void testNotTooComplex() {
+  void testLoopAfterForDoesNotCountAsExtraNestingLevel() {
+    final NestingLevelCheck check = new NestingLevelCheck();
+
+    final String code =
+        """
+        _method a.b
+            _if a
+            _then
+                _for i _over 1.upto(5)
+                _loop
+                    _if b
+                    _then
+                    _endif
+                _endif
+            _endif
+        _endmethod
+        """;
+    final List<MagikIssue> issues = this.runCheck(code, check);
+    // Should be empty since the `_loop` is at the same level as the `_for`.
+    assertThat(issues).isEmpty();
+  }
+
+  @Test
+  void testNotExceedingMaximumNestingLevel() {
     final MagikCheck check = new NestingLevelCheck();
     final String code =
         """
