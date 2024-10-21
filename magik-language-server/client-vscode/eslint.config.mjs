090c73134c421327955196bf06580194059f09ad
Steven Looman <steven.looman@gmail.com>
2024-10-21 22:01:29 +0200

    Upgrade ESLint

---
+++ magik-language-server/client-vscode/eslint.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config({
+    files: ['**/*.ts'],
+    extends: [
+        eslint.configs.recommended,
+        ...tseslint.configs.strict,
+        ...tseslint.configs.stylistic,
+    ]
+});
