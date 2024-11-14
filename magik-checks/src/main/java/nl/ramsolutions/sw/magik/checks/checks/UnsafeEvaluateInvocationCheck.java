9d9ed79706d181b0aa0503c61bb41cd2aad35480
Sebastiaan Speck <shem.speck@gmail.com>
2024-11-14 23:18:02 +0100

    Use new helper method

--- magik-checks/src/main/java/nl/ramsolutions/sw/magik/checks/checks/UnsafeEvaluateInvocationCheck.java
+++ magik-checks/src/main/java/nl/ramsolutions/sw/magik/checks/checks/UnsafeEvaluateInvocationCheck.java
@@ -23,6 +23,7 @@ public class UnsafeEvaluateInvocationCheck extends MagikCheck {
       return;
     }
 
-    this.addIssue(node.getFirstChild(MagikGrammar.METHOD_NAME), MESSAGE);
+    final AstNode methodNameNode = helper.getMethodNameNode();
+    this.addIssue(methodNameNode, MESSAGE);
   }
 }
