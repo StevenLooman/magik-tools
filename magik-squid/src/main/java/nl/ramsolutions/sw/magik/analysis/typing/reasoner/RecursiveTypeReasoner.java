3e48bf3e7035efee5b3b47797cfe601e95910b24
Steven Looman <steven.looman@gmail.com>
2025-01-10 17:49:46 +0100

    Add initial version of RecursiveTypeReasoner

---
+++ magik-squid/src/main/java/nl/ramsolutions/sw/magik/analysis/typing/reasoner/RecursiveTypeReasoner.java
@@ -0,0 +1,253 @@
+package nl.ramsolutions.sw.magik.analysis.typing.reasoner;
+
+import com.sonar.sslr.api.AstNode;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Objects;
+import nl.ramsolutions.sw.FileCharsetDeterminer;
+import nl.ramsolutions.sw.MagikToolsProperties;
+import nl.ramsolutions.sw.magik.Location;
+import nl.ramsolutions.sw.magik.MagikTypedFile;
+import nl.ramsolutions.sw.magik.analysis.MagikAnalysisSettings;
+import nl.ramsolutions.sw.magik.analysis.definitions.IDefinitionKeeper;
+import nl.ramsolutions.sw.magik.analysis.definitions.MethodDefinition;
+import nl.ramsolutions.sw.magik.analysis.definitions.MethodUsage;
+import nl.ramsolutions.sw.magik.analysis.helpers.MethodInvocationNodeHelper;
+import nl.ramsolutions.sw.magik.analysis.typing.ExpressionResultString;
+import nl.ramsolutions.sw.magik.analysis.typing.SelfHelper;
+import nl.ramsolutions.sw.magik.analysis.typing.TypeString;
+import nl.ramsolutions.sw.magik.analysis.typing.TypeStringResolver;
+import nl.ramsolutions.sw.magik.api.MagikGrammar;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Recursive type reasoner.
+ *
+ * <p>Reasons about definitions, recursively following any used methods.
+ */
+public class RecursiveTypeReasoner {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(RecursiveTypeReasoner.class);
+
+  private final IDefinitionKeeper definitionKeeper;
+  private final int maxDepth;
+
+  /**
+   * Constructor.
+   *
+   * @param maxDepth the maximum depth to reason.
+   */
+  public RecursiveTypeReasoner(final IDefinitionKeeper definitionKeeper, final int maxDepth) {
+    this.definitionKeeper = definitionKeeper;
+    this.maxDepth = maxDepth;
+  }
+
+  /**
+   * Reason the method definition to found the return type, recursively following any used methods,
+   * up to {@link maxDepth} depth.
+   *
+   * <p>The {@link MethodDefinition}s in the {@link IDefinitionKeeper} will be replaced in case the
+   * {@link MethodDefinition} return types were not UNDEFINED and could be reasoned from the current
+   * code.
+   *
+   * @param methodDefinition The {@link MethodDefinition} to reason.
+   * @return True if the method definition return type could be reasoned, false otherwise.
+   */
+  public boolean reason(final MethodDefinition methodDefinition) {
+    this.reason(methodDefinition, 0);
+
+    // Test if method definition now has a return type.
+    final TypeString typeStr = methodDefinition.getTypeName();
+    final String methodName = methodDefinition.getName();
+    final MethodDefinition updatedMethodDefinition =
+        this.definitionKeeper.getMethodDefinitions(typeStr).stream()
+            .filter(methodDef -> methodDef.getName().equals(methodName))
+            .filter(methodDef -> methodDef.getLocation().equals(methodDefinition.getLocation()))
+            .findAny()
+            .orElseThrow();
+    return updatedMethodDefinition.getReturnTypes() != ExpressionResultString.UNDEFINED;
+  }
+
+  private void reason(final MethodDefinition methodDefinition, final int depth) {
+    LOGGER.debug("Reasoning method definition: {}, depth: {}", methodDefinition, depth);
+
+    if (depth > this.maxDepth) {
+      // Don't go further than maxDepth.
+      return;
+    }
+
+    if (methodDefinition.getReturnTypes() != ExpressionResultString.UNDEFINED) {
+      // Already reasoned this method definition, or from TypeDoc.
+      return;
+    }
+
+    final Location location = methodDefinition.getLocation();
+    if (location == null) {
+      // Don't have any source code to reason from.
+      return;
+    }
+
+    // Find method definition in the file.
+    final MagikTypedFile magikFile1 = this.getMagikFile(location);
+    final MethodDefinition fileMethodDefinition1 =
+        this.getMethodDefinitionFromFile(methodDefinition, magikFile1);
+
+    // Reason method and test if the returned type is not UNDEFINED now.
+    final LocalTypeReasonerState reasonerState1 = magikFile1.getTypeReasonerState();
+    final AstNode node1 = fileMethodDefinition1.getNode();
+    final ExpressionResultString nodeType1 = reasonerState1.getNodeType(node1);
+    final ExpressionResultString nodeIterType1 = reasonerState1.getNodeIterType(node1);
+    if (nodeType1 != ExpressionResultString.UNDEFINED
+        && nodeIterType1 != ExpressionResultString.UNDEFINED) {
+      // Update the MethodDefinition with the known return type.
+      this.updateMethodDefinitionTypes(
+          methodDefinition, fileMethodDefinition1, nodeType1, nodeIterType1);
+
+      return;
+    }
+
+    // If returned type is UNDEFINED, recurse from the METHOD_INVOCATION nodes.
+    // This might make the return type known.
+    Collection<MethodUsage> previousUndefinedMethodUsages = Collections.emptySet();
+    Collection<MethodUsage> undefinedMethodUsages =
+        this.extractUndefinedMethodUsages(magikFile1, node1);
+    final TypeStringResolver resolver = magikFile1.getTypeStringResolver();
+    while (!previousUndefinedMethodUsages.equals(undefinedMethodUsages)) {
+      undefinedMethodUsages.stream()
+          .map(usage -> this.getMethodDefinitions(resolver, usage))
+          .flatMap(Collection::stream)
+          .forEach(usedMethodDefinition -> this.reason(usedMethodDefinition, depth + 1));
+      previousUndefinedMethodUsages = undefinedMethodUsages;
+      undefinedMethodUsages = this.extractUndefinedMethodUsages(magikFile1, node1);
+    }
+
+    // After recusing, we need to re-try this method again.
+    // Find method definition in the file.
+    final MagikTypedFile magikFile2 = this.getMagikFile(location);
+    final MethodDefinition fileMethodDefinition2 =
+        this.getMethodDefinitionFromFile(methodDefinition, magikFile2);
+
+    // Reason method and test if the returned type is not UNDEFINED now.
+    final LocalTypeReasonerState reasonerState2 = magikFile2.getTypeReasonerState();
+    final AstNode node2 = fileMethodDefinition2.getNode();
+    final ExpressionResultString nodeType2 = reasonerState2.getNodeType(node2);
+    final ExpressionResultString nodeIterType2 = reasonerState2.getNodeIterType(node2);
+    if (nodeType2 != ExpressionResultString.UNDEFINED
+        && nodeIterType2 != ExpressionResultString.UNDEFINED) {
+      // Update the MethodDefinition with the known return type.
+      this.updateMethodDefinitionTypes(
+          methodDefinition, fileMethodDefinition2, nodeType2, nodeIterType2);
+    }
+  }
+
+  private MethodDefinition getMethodDefinitionFromFile(
+      final MethodDefinition methodDefinition, final MagikTypedFile magikFile) {
+    final Location location = methodDefinition.getLocation();
+    final String methodDefinitionName = methodDefinition.getName();
+    return magikFile.getDefinitions().stream()
+        .filter(MethodDefinition.class::isInstance)
+        .map(MethodDefinition.class::cast)
+        .filter(def -> def.getName().equals(methodDefinitionName))
+        .filter(def -> def.getLocation().equals(location))
+        .findAny()
+        .orElseThrow();
+  }
+
+  private Collection<MethodUsage> extractUndefinedMethodUsages(
+      final MagikTypedFile magikFile, final AstNode node) {
+    final LocalTypeReasonerState reasonerState = magikFile.getTypeReasonerState();
+    return node.getDescendants(MagikGrammar.METHOD_INVOCATION).stream()
+        .map(
+            methodInvocationNode -> {
+              // TODO: Iterator methods?
+              final ExpressionResultString invocationResult =
+                  reasonerState.getNodeType(methodInvocationNode);
+              if (invocationResult != ExpressionResultString.UNDEFINED) {
+                // Already known, no need to recurse.
+                return null;
+              }
+
+              final MethodInvocationNodeHelper helper =
+                  new MethodInvocationNodeHelper(methodInvocationNode);
+              final AstNode receiverNode = helper.getReceiverNode();
+              final ExpressionResultString receiverType = reasonerState.getNodeType(receiverNode);
+              if (receiverType == ExpressionResultString.UNDEFINED) {
+                // Cannot do anything with this.
+                return null;
+              }
+              final TypeString resultTypeStr = receiverType.get(0, TypeString.UNDEFINED);
+              final TypeString typeStr =
+                  SelfHelper.substituteSelf(resultTypeStr, methodInvocationNode);
+              if (typeStr.isUndefined()) {
+                // Cannot do anything with this.
+                return null;
+              }
+
+              final String invokedMethodName = helper.getMethodName();
+              return new MethodUsage(typeStr, invokedMethodName);
+            })
+        .filter(Objects::nonNull)
+        .distinct()
+        .toList();
+  }
+
+  private void updateMethodDefinitionTypes(
+      final MethodDefinition methodDefinition,
+      final MethodDefinition fileMethodDefinition,
+      final ExpressionResultString nodeType,
+      final ExpressionResultString nodeIterType) {
+    final MethodDefinition updatedMethodDefinition =
+        new MethodDefinition(
+            fileMethodDefinition.getLocation(),
+            fileMethodDefinition.getTimestamp(),
+            fileMethodDefinition.getModuleName(),
+            fileMethodDefinition.getDoc(),
+            null,
+            fileMethodDefinition.getTypeName(),
+            fileMethodDefinition.getMethodName(),
+            fileMethodDefinition.getModifiers(),
+            fileMethodDefinition.getParameters(),
+            fileMethodDefinition.getAssignmentParameter(),
+            fileMethodDefinition.getTopics(),
+            nodeType,
+            nodeIterType);
+
+    // Save the new MethodDefinition.
+    this.definitionKeeper.remove(methodDefinition);
+    this.definitionKeeper.add(updatedMethodDefinition);
+  }
+
+  private MagikTypedFile getMagikFile(final Location location) {
+    final MagikToolsProperties properties =
+        new MagikToolsProperties(
+            Map.of(
+                MagikAnalysisSettings.INDEX_GLOBAL_USAGES, "true",
+                MagikAnalysisSettings.INDEX_METHOD_USAGES, "true",
+                MagikAnalysisSettings.INDEX_SLOT_USAGES, "true",
+                MagikAnalysisSettings.INDEX_CONDITION_USAGES, "true"));
+    final URI uri = location.getUri();
+    final Path path = Path.of(uri);
+    final Charset charset = FileCharsetDeterminer.determineCharset(path);
+    final String text;
+    try {
+      text = Files.readString(path, charset);
+    } catch (final IOException exception) {
+      throw new IllegalStateException(exception);
+    }
+    return new MagikTypedFile(properties, uri, text, this.definitionKeeper);
+  }
+
+  private Collection<MethodDefinition> getMethodDefinitions(
+      final TypeStringResolver resolver, final MethodUsage methodUsage) {
+    final TypeString usedTypeName = methodUsage.getTypeName();
+    final String usedMethodName = methodUsage.getMethodName();
+    return resolver.getRespondingMethodDefinitions(usedTypeName, usedMethodName);
+  }
+}
---
+++ magik-squid/src/test/java/nl/ramsolutions/sw/magik/analysis/typing/reasoner/RecursiveTypeReasonerTest.java
@@ -0,0 +1,105 @@
+package nl.ramsolutions.sw.magik.analysis.typing.reasoner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.net.URI;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import nl.ramsolutions.sw.magik.MagikTypedFile;
+import nl.ramsolutions.sw.magik.analysis.definitions.DefinitionKeeper;
+import nl.ramsolutions.sw.magik.analysis.definitions.IDefinitionKeeper;
+import nl.ramsolutions.sw.magik.analysis.definitions.MethodDefinition;
+import nl.ramsolutions.sw.magik.analysis.typing.ExpressionResultString;
+import nl.ramsolutions.sw.magik.analysis.typing.TypeString;
+import org.junit.jupiter.api.Test;
+
+class RecursiveTypeReasonerTest {
+
+  /**
+   * VSCode runs from module directory, mvn runs from project directory.
+   *
+   * @return Proper {@link Path} to file.
+   */
+  protected Path getPath(final Path relativePath) {
+    final Path path = Path.of(".").toAbsolutePath().getParent();
+    if (path.endsWith("magik-squid")) {
+      return Path.of("..").resolve(relativePath);
+    }
+
+    return Path.of(".").resolve(relativePath);
+  }
+
+  protected void addFileToDefinitionKeeper(
+      final IDefinitionKeeper definitionKeeper, final Path path) throws IOException {
+    final URI uri = path.toUri();
+    final String content = Files.readString(path, Charset.defaultCharset());
+    final MagikTypedFile magikTypedFile = new MagikTypedFile(uri, content, definitionKeeper);
+    magikTypedFile.getDefinitions().forEach(definitionKeeper::add);
+  }
+
+  @Test
+  void testRecursiveReasoningDepth1() throws IOException {
+    final IDefinitionKeeper definitionKeeper = new DefinitionKeeper();
+    final Path path = Path.of("magik-squid/src/test/resources/test_recursive_type_reasoner.magik");
+    final Path fixedPath = this.getPath(path);
+    this.addFileToDefinitionKeeper(definitionKeeper, fixedPath);
+
+    // Ensure typing isn't known currently.
+    final TypeString typeStr = TypeString.ofIdentifier("test_exemplar", "user");
+    final MethodDefinition methodDefinition =
+        definitionKeeper.getMethodDefinitions(typeStr).stream()
+            .filter(methodDef -> methodDef.getMethodName().equals("depth2()"))
+            .findFirst()
+            .orElseThrow();
+    final ExpressionResultString resultStr = methodDefinition.getReturnTypes();
+    assertThat(resultStr).isEqualTo(ExpressionResultString.UNDEFINED);
+
+    // Recusively reason the method definition.
+    final RecursiveTypeReasoner recursiveTypeReasoner =
+        new RecursiveTypeReasoner(definitionKeeper, 1);
+    recursiveTypeReasoner.reason(methodDefinition);
+
+    // Test if the method definition now has a return type.
+    final MethodDefinition updatedMethodDefinition =
+        definitionKeeper.getMethodDefinitions(typeStr).stream()
+            .filter(methodDef -> methodDef.getMethodName().equals("depth2()"))
+            .findFirst()
+            .orElseThrow();
+    final ExpressionResultString updatedResultStr = updatedMethodDefinition.getReturnTypes();
+    assertThat(updatedResultStr).isEqualTo(new ExpressionResultString(TypeString.SELF));
+  }
+
+  @Test
+  void testRecursiveReasoningOverMaxDepth() throws IOException {
+    final IDefinitionKeeper definitionKeeper = new DefinitionKeeper();
+    final Path path = Path.of("magik-squid/src/test/resources/test_recursive_type_reasoner.magik");
+    final Path fixedPath = this.getPath(path);
+    this.addFileToDefinitionKeeper(definitionKeeper, fixedPath);
+
+    // Ensure typing isn't known currently.
+    final TypeString typeStr = TypeString.ofIdentifier("test_exemplar", "user");
+    final MethodDefinition methodDefinition =
+        definitionKeeper.getMethodDefinitions(typeStr).stream()
+            .filter(methodDef -> methodDef.getMethodName().equals("depth4()"))
+            .findFirst()
+            .orElseThrow();
+    final ExpressionResultString resultStr = methodDefinition.getReturnTypes();
+    assertThat(resultStr).isEqualTo(ExpressionResultString.UNDEFINED);
+
+    // Recusively reason the method definition.
+    final RecursiveTypeReasoner recursiveTypeReasoner =
+        new RecursiveTypeReasoner(definitionKeeper, 2);
+    recursiveTypeReasoner.reason(methodDefinition);
+
+    // Test if the method definition is not reasoned, as it surpasses max depth.
+    final MethodDefinition updatedMethodDefinition =
+        definitionKeeper.getMethodDefinitions(typeStr).stream()
+            .filter(methodDef -> methodDef.getMethodName().equals("depth4()"))
+            .findFirst()
+            .orElseThrow();
+    final ExpressionResultString updatedResultStr = updatedMethodDefinition.getReturnTypes();
+    assertThat(updatedResultStr).isEqualTo(ExpressionResultString.UNDEFINED);
+  }
+}
