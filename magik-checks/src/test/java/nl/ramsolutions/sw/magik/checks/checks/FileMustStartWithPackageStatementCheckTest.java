7fc7257dab740c8d11ff39683a8fe1152995e71b
Steven Looman <steven.looman@gmail.com>
2025-03-03 21:02:07 +0100

    Fix profile

--- magik-checks/src/test/java/nl/ramsolutions/sw/magik/checks/checks/FileMustStartWithPackageStatementCheckTest.java
+++ magik-checks/src/test/java/nl/ramsolutions/sw/magik/checks/checks/FileMustStartWithPackageStatementCheckTest.java
@@ -8,7 +8,7 @@ import nl.ramsolutions.sw.magik.checks.MagikIssue;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-/** Test FileWithoutPackageStatementCheck. */
+/** Test FileMustStartWithPackageStatementCheckTest. */
 class FileMustStartWithPackageStatementCheckTest extends MagikCheckTestBase {
 
   @ParameterizedTest
