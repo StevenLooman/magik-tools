145e77ca4ad464a5ec8ba78b3c43d3803a0d62f9
Steven Looman <steven.looman@gmail.com>
2025-06-22 15:54:21 +0200

    Fix `HidesVariableCheck` not seeing `DEFINITION` `ScopeEntry`s

--- magik-checks/src/test/java/nl/ramsolutions/sw/magik/checks/checks/VariableNamingCheckTest.java
+++ magik-checks/src/test/java/nl/ramsolutions/sw/magik/checks/checks/VariableNamingCheckTest.java
@@ -9,23 +9,60 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-/** Test VariableNamingCheck. */
+/** Test {@link VariableNamingCheck}. */
 class VariableNamingCheckTest extends MagikCheckTestBase {
 
   @ParameterizedTest
   @ValueSource(
       strings = {
-        "_local coord",
-        "coord << 10",
-        "_local x",
-        "_local id",
-        "_method a.b(coord) _endmethod",
-        "_local l_coord",
-        "_local l_x",
-        "_local l_id",
-        "_local (l_item, l_result) << (1, 2)",
-        "result +<< 10",
-        "result +<< str << _self.a",
+        """
+        _block
+          _local coord
+        _endblock
+        """,
+        """
+        coord << 10
+        """,
+        """
+        _block
+          _local x
+        _endblock
+        """,
+        """
+        _block
+          _local id
+        _endblock
+        """,
+        """
+        _method a.b(coord)
+        _endmethod
+        """,
+        """
+        _block
+          _local l_coord
+        _endblock
+        """,
+        """
+        _block
+          _local l_x
+        _endblock
+        """,
+        """
+        _block
+          _local l_id
+        _endblock
+        """,
+        """
+        _block
+          _local (l_item, l_result) << (1, 2)
+        _endblock
+        """,
+        """
+        result +<< 10
+        """,
+        """
+        result +<< str << _self.a
+        """,
       })
   void testValid(final String code) {
     final MagikCheck check = new VariableNamingCheck();
@@ -94,7 +131,12 @@ class VariableNamingCheckTest extends MagikCheckTestBase {
   @Test
   void testMultiVariableDeclarationInvalidName() {
     final MagikCheck check = new VariableNamingCheck();
-    final String code = "_local (l_i, l_r) << (1, 2)";
+    final String code =
+        """
+    _block
+      _local (l_i, l_r) << (1, 2)
+    _endblock
+    """;
     final List<MagikIssue> issues = this.runCheck(code, check);
     assertThat(issues).hasSize(2);
   }
