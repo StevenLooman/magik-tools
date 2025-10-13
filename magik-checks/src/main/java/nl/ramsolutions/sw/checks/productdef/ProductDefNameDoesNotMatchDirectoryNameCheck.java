f0d48db4601573360a400b9ce2b184612667d3fe
Steven Looman <steven.looman@gmail.com>
2025-10-13 23:48:05 +0200

    Add ProductDefNameDoesNotMatchDirectoryNameCheck

---
+++ magik-checks/src/main/java/nl/ramsolutions/sw/checks/productdef/ProductDefNameDoesNotMatchDirectoryNameCheck.java
@@ -0,0 +1,43 @@
+package nl.ramsolutions.sw.checks.productdef;
+
+import com.sonar.sslr.api.AstNode;
+import java.net.URI;
+import java.nio.file.Path;
+import nl.ramsolutions.sw.checks.ProductDefCheck;
+import nl.ramsolutions.sw.productdef.ProductDefFile;
+import nl.ramsolutions.sw.productdef.api.ProductDefinitionGrammar;
+import org.sonar.check.Rule;
+
+/** Check that a product definition matches the directory name. */
+@Rule(key = ProductDefNameDoesNotMatchDirectoryNameCheck.CHECK_KEY)
+public class ProductDefNameDoesNotMatchDirectoryNameCheck extends ProductDefCheck {
+
+  @SuppressWarnings("checkstyle:JavadocVariable")
+  public static final String CHECK_KEY = "ProductNameDoesNotMatchDirectoryName";
+
+  private static final String MESSAGE = "Product name does not match directory name.";
+
+  @Override
+  protected void scanFile() {
+    final ProductDefFile productDefFile = this.getProductDefFile();
+    final AstNode topNode = productDefFile.getTopNode();
+    final AstNode productNameNode =
+        topNode.getFirstDescendant(ProductDefinitionGrammar.PRODUCT_NAME);
+    if (productNameNode == null) {
+      return;
+    }
+
+    final String productName = productNameNode.getTokenValue();
+    final URI uri = productDefFile.getUri();
+    final Path path = Path.of(uri);
+    final Path parentPath = path.getParent();
+    if (parentPath == null) {
+      return;
+    }
+
+    final String directoryName = parentPath.getFileName().toString();
+    if (!productName.equalsIgnoreCase(directoryName)) {
+      this.addIssue(productNameNode, MESSAGE);
+    }
+  }
+}
---
+++ magik-checks/src/test/java/nl/ramsolutions/sw/checks/productdef/ProductDefNameDoesNotMatchDirectoryNameCheckTest.java
@@ -0,0 +1,27 @@
+package nl.ramsolutions.sw.checks.productdef;
+
+import static nl.ramsolutions.sw.checks.productdef.ProductDefCheckAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import nl.ramsolutions.sw.checks.ProductDefCheck;
+import org.junit.jupiter.api.Test;
+
+/** Test {@link ProductDefNameDoesNotMatchDirectoryNameCheck}. */
+class ProductDefNameDoesNotMatchDirectoryNameCheckTest {
+
+  @Test
+  void testOk() throws IllegalArgumentException, IOException {
+    final ProductDefCheck check = new ProductDefNameDoesNotMatchDirectoryNameCheck();
+    final Path path = Path.of("magik-checks/src/test/resources/test_product/product.def");
+    assertThat(check).reportsNoIssues(path);
+  }
+
+  @Test
+  void testInvalid() throws IllegalArgumentException, IOException {
+    final ProductDefCheck check = new ProductDefNameDoesNotMatchDirectoryNameCheck();
+    final Path path = Path.of("magik-checks/src/test/resources/test_product_2/product.def");
+    assertThat(check).reportsIssueCount(path, 1);
+  }
+}
