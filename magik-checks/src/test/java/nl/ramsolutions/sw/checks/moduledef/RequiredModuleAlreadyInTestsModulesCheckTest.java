f08268019465498e0e9652bb04b56f2b0a8eb0d1
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2025-10-18 09:28:51 +0200

    Add tests for RequiredModuleAlreadyInTestsModulesCheck

---
+++ magik-checks/src/test/java/nl/ramsolutions/sw/checks/moduledef/RequiredModuleAlreadyInTestsModulesCheckTest.java
@@ -0,0 +1,92 @@
+package nl.ramsolutions.sw.checks.moduledef;
+
+import static nl.ramsolutions.sw.checks.moduledef.ModuleDefCheckAssert.assertThat;
+
+import nl.ramsolutions.sw.checks.ModuleDefCheck;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+/** Test {@link RequiredModuleAlreadyInTestsModulesCheck}. */
+class RequiredModuleAlreadyInTestsModulesCheckTest {
+
+  @ParameterizedTest(name = "Valid module.def example {index}")
+  @ValueSource(strings = {
+    """
+    example_module_test 1
+
+    requires
+        other_module
+        another_module
+    end
+
+    tests_modules
+        example_module
+    end
+    """,
+    """
+    example_module_test 1
+
+    tests_modules
+        example_module
+    end
+    """,
+    """
+    example_module_test 1
+
+    requires
+    end
+
+    tests_modules
+        example_module
+    end
+    """,
+    """
+    example_module_test 1
+
+    requires
+        other_module
+        another_module
+    end
+
+    tests_modules
+
+    end
+    """,
+    """
+    example_module_test 1
+
+    requires
+        other_module
+        another_module
+        example_module
+    end
+
+    tests_modules
+
+    end
+    """,
+  })
+  void testOk(final String code) {
+    final ModuleDefCheck check = new RequiredModuleAlreadyInTestsModulesCheck();
+    assertThat(check).reportsNoIssues(code);
+  }
+
+  @Test
+  void testInvalid() {
+    final ModuleDefCheck check = new RequiredModuleAlreadyInTestsModulesCheck();
+    final String code =
+        """
+        example_module_tests 1
+
+        requires
+            example_module
+        end
+
+        tests_modules
+            example_module
+        end
+        """;
+    assertThat(check).reportsIssueCount(code, 1);
+  }
+}
