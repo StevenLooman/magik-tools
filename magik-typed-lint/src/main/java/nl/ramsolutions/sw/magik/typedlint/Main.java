5b8e3775952e3edcfab958eb280d72098faa9025
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2025-10-21 22:38:10 +0200

    Update help message in Main.java

--- magik-typed-lint/src/main/java/nl/ramsolutions/sw/magik/typedlint/Main.java
+++ magik-typed-lint/src/main/java/nl/ramsolutions/sw/magik/typedlint/Main.java
@@ -265,7 +265,7 @@ public final class Main {
     // Help.
     if (commandLine.hasOption(OPTION_HELP) || commandLine.getArgs().length == 0) {
       final HelpFormatter helpFormatter = HelpFormatter.builder().setShowSince(false).get();
-      helpFormatter.printHelp("java -jar magik-typed-lint.jar", "", Main.OPTIONS, "", true);
+      helpFormatter.printHelp("java -jar magik-typed-lint.jar", "magik-typed-lint", Main.OPTIONS, "", true);
 
       System.exit(0);
     }
