840a8619cfe2cd7713aafba2b6a703cbf53f7114
Steven Looman <steven.looman@gmail.com>
2025-10-22 22:04:05 +0200

    Add RemoveUnusedImports recipe

--- magik-checks/src/test/java/nl/ramsolutions/sw/checks/magik/VariableDeclarationUsageDistanceCheckTest.java
+++ magik-checks/src/test/java/nl/ramsolutions/sw/checks/magik/VariableDeclarationUsageDistanceCheckTest.java
@@ -1,7 +1,6 @@
 package nl.ramsolutions.sw.checks.magik;
 
 import static nl.ramsolutions.sw.checks.magik.MagikCheckAssert.assertThat;
-import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
