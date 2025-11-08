93e1cbf3f2998ab8698bd0c506ecbc1e9a6755fc
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2025-11-08 21:17:21 +0100

    Add tests for FormattingFixer and UseValueCompareFixer (#376)

--- magik-checks/src/test/java/nl/ramsolutions/sw/checks/magik/UseValueCompareCheckTest.java
+++ magik-checks/src/test/java/nl/ramsolutions/sw/checks/magik/UseValueCompareCheckTest.java
@@ -13,8 +13,13 @@ class UseValueCompareCheckTest {
   @ValueSource(
       strings = {
         "a _is b",
+        "a _isnt b",
         "a = \"b\"",
+        "a <> \"b\"",
         "a _is 1",
+        "a _isnt 1",
+        "536870913 = a",
+        "536870913 <> a",
       })
   void testValid(final String code) {
     final MagikCheck check = new UseValueCompareCheck();
@@ -25,10 +30,15 @@ class UseValueCompareCheckTest {
   @ValueSource(
       strings = {
         "\"a\" _is b",
+        "\"a\" _isnt b",
         "a _is \"b\"",
+        "a _isnt \"b\"",
         "a _is 0.0",
+        "a _isnt 0.0",
         "536870913 _is a",
+        "536870913 _isnt a",
         "a _is 16rffffffffffff",
+        "a _isnt 16rffffffffffff",
       })
   void testInvalid(final String code) {
     final MagikCheck check = new UseValueCompareCheck();
