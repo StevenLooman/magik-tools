3735b36d068f648898acb3f765e318ccb9bb435b
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2025-11-09 13:01:12 +0000

    Use ParameterizedTest

--- magik-checks/src/test/java/nl/ramsolutions/sw/checks/magik/PragmaDoesNotIncludeTopicInTopicsCheckTest.java
+++ magik-checks/src/test/java/nl/ramsolutions/sw/checks/magik/PragmaDoesNotIncludeTopicInTopicsCheckTest.java
@@ -6,26 +6,22 @@ import java.io.IOException;
 import java.nio.file.Path;
 import nl.ramsolutions.sw.checks.MagikCheck;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /** Tests {@link PragmaDoesNotIncludeTopicInTopicsCheck}. */
 public class PragmaDoesNotIncludeTopicInTopicsCheckTest {
 
-  @Test
-  void testValid1() throws IOException {
+  @ParameterizedTest
+  @ValueSource(
+      strings = {
+        "magik-checks/src/test/resources/test_product_2/modules/test_module_2/source/test_exemplar.magik",
+        "magik-checks/src/test/resources/test_product/modules/test_module/source/test_exemplar.magik"
+      })
+  void testValid(final String path) throws IOException {
     final MagikCheck check = new PragmaDoesNotIncludeTopicInTopicsCheck();
-    final Path path =
-        Path.of(
-            "magik-checks/src/test/resources/test_product_2/modules/test_module_2/source/test_exemplar.magik");
-    assertThat(check).reportsNoIssues(path);
-  }
-
-  @Test
-  void testValid2() throws IOException {
-    final MagikCheck check = new PragmaDoesNotIncludeTopicInTopicsCheck();
-    final Path path =
-        Path.of(
-            "magik-checks/src/test/resources/test_product/modules/test_module/source/test_exemplar.magik");
-    assertThat(check).reportsNoIssues(path);
+    final Path filePath = Path.of(path);
+    assertThat(check).reportsNoIssues(filePath);
   }
 
   @Test
