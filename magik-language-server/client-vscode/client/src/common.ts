9b1c935257c5200288ad7379a7039f29d85bccf5
Steven Looman <steven.looman@gmail.com>
2026-02-14 22:41:03 +0100

    Fix message when java cannot be located, with minor refactoring

--- magik-language-server/client-vscode/client/src/common.ts
+++ magik-language-server/client-vscode/client/src/common.ts
@@ -2,16 +2,29 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
-export function getJavaExec(): fs.PathLike {
+export function getJavaExecSafe(): fs.PathLike | null {
 	const javaHomeEnv = process.env['JAVA_HOME'];
 	const javaHome = vscode.workspace.getConfiguration().get('magik.javaHome', javaHomeEnv);
 	if (javaHome == null) {
 		return null;
 	}
+
 	const javaName = process.platform === "win32" ? 'java.exe' : 'java';
 	const javaExec = path.join(javaHome, 'bin', javaName);
 	if (!fs.existsSync(javaExec)) {
 		return null;
 	}
+
+	return javaExec;
+}
+
+export function getJavaExec(): fs.PathLike {
+	const javaExec = getJavaExecSafe();
+	if (javaExec == null) {
+		const message = 'Could not locate java executable, either set Java Home setting ("magik.javaHome") or JAVA_HOME environment variable.';
+		vscode.window.showWarningMessage(message);
+		throw new Error(message);
+	}
+
 	return javaExec;
 }
--- magik-language-server/client-vscode/client/src/debug-provider.ts
+++ magik-language-server/client-vscode/client/src/debug-provider.ts
@@ -28,10 +28,6 @@ class DebugAdapterExecutableFactory implements vscode.DebugAdapterDescriptorFact
 
 	createDebugAdapterDescriptor(_session: vscode.DebugSession, _executable: vscode.DebugAdapterExecutable | undefined): vscode.ProviderResult<vscode.DebugAdapterDescriptor> {
 		const javaExec = getJavaExec();
-		if (javaExec == null) {
-			vscode.window.showWarningMessage('Could locate java executable, either set Java Home setting ("magik.javaHome") or JAVA_HOME environment variable.');
-			return;
-		}
 		const jar = path.join(__dirname, '..', '..', 'server', 'magik-debug-adapter-' + MAGIK_TOOLS_VERSION + '.jar');
 		const javaDebuggerOptions = '-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,quiet=y,address=127.0.0.1:5006';
 
