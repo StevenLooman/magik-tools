5494a88cfe6beceaa3c224d7ffa4824fbd5ebf81
Sebastiaan Speck <12570668+sebastiaanspeck@users.noreply.github.com>
2026-03-14 10:00:32 +0100

    Add and align `--version/--help` options to all applicable modules (#498)

--- magik-language-server/src/main/java/nl/ramsolutions/sw/magik/languageserver/Main.java
+++ magik-language-server/src/main/java/nl/ramsolutions/sw/magik/languageserver/Main.java
@@ -2,6 +2,7 @@ package nl.ramsolutions.sw.magik.languageserver;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.util.logging.LogManager;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.CommandLineParser;
@@ -9,6 +10,7 @@ import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.help.HelpFormatter;
 import org.eclipse.lsp4j.jsonrpc.Launcher;
 import org.eclipse.lsp4j.launch.LSPLauncher;
 import org.eclipse.lsp4j.services.LanguageClient;
@@ -24,14 +26,40 @@ public final class Main {
           .longOpt("stdio")
           .desc("Use STDIO (default, no other option to interface with this language server)")
           .get();
+  private static final Option OPTION_VERSION =
+      Option.builder().longOpt("version").desc("Show version and exit").get();
+  private static final Option OPTION_HELP =
+      Option.builder().longOpt("help").desc("Show this help and exit").get();
 
   static {
     OPTIONS = new Options();
     OPTIONS.addOption(OPTION_DEBUG);
     OPTIONS.addOption(OPTION_STDIO);
+    OPTIONS.addOption(OPTION_VERSION);
+    OPTIONS.addOption(OPTION_HELP);
   }
 
   private Main() {}
+
+  private static PrintStream getOutStream() {
+    return System.out; // NOSONAR
+  }
+
+  private static InputStream getInStream() {
+    return System.in; // NOSONAR
+  }
+
+  private static String getName() {
+    return "magik-language-server";
+  }
+
+  private static String getArtifactName() {
+    return Main.getName() + ".jar";
+  }
+
+  private static String getVersion() {
+    return Main.class.getPackage().getImplementationVersion();
+  }
 
   /**
    * Initialize logger from logging.properties.
@@ -82,13 +110,41 @@ public final class Main {
       Main.initLogger();
     }
 
+    if (commandLine.hasOption(OPTION_HELP)) {
+      Main.showHelp();
+
+      System.exit(0);
+    }
+
+    if (commandLine.hasOption(OPTION_VERSION)) {
+      Main.showVersion();
+
+      System.exit(0);
+    }
+
     final MagikLanguageServer server = new MagikLanguageServer();
+    final InputStream inStream = Main.getInStream();
+    final PrintStream outStream = Main.getOutStream();
     final Launcher<LanguageClient> launcher =
-        LSPLauncher.createServerLauncher(server, System.in, System.out); // NOSONAR
+        LSPLauncher.createServerLauncher(server, inStream, outStream);
 
     final LanguageClient remoteProxy = launcher.getRemoteProxy();
     server.connect(remoteProxy);
 
     launcher.startListening();
+  }
+
+  private static void showHelp() throws IOException {
+    final HelpFormatter helpFormatter = HelpFormatter.builder().setShowSince(false).get();
+    final String artifactName = Main.getArtifactName();
+    final String name = Main.getName();
+    helpFormatter.printHelp(
+        "java -jar " + artifactName, name + "\s" + Main.getVersion(), Main.OPTIONS, "", true);
+  }
+
+  private static void showVersion() {
+    final String version = Main.getVersion();
+    final PrintStream outStream = Main.getOutStream();
+    outStream.println(version);
   }
 }
