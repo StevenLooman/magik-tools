61d60950e425065e08fa2c7603d2bf0a793fbfd2
Steven Looman <steven.looman@gmail.com>
2026-07-28 14:11:14 +0200

    Report `@return`/`@loop` entries that carry no type (#647)

--- magik-checks/src/test/java/nl/ramsolutions/sw/checks/magiktyped/IterCallableYieldTypesMatchDocTypedCheckTest.java
+++ magik-checks/src/test/java/nl/ramsolutions/sw/checks/magiktyped/IterCallableYieldTypesMatchDocTypedCheckTest.java
@@ -5,6 +5,9 @@ import static nl.ramsolutions.sw.checks.magiktyped.MagikTypedCheckAssert.assertT
 import nl.ramsolutions.sw.checks.MagikTypedCheck;
 import nl.ramsolutions.sw.magik.analysis.definitions.DefinitionKeeper;
 import nl.ramsolutions.sw.magik.analysis.definitions.IDefinitionKeeper;
+import nl.ramsolutions.sw.magik.analysis.definitions.MethodDefinition;
+import nl.ramsolutions.sw.magik.analysis.typing.ExpressionResultString;
+import nl.ramsolutions.sw.magik.analysis.typing.TypeString;
 import org.junit.jupiter.api.Test;
 
 /** Tests for {@link IterCallableYieldTypesMatchDocTypedCheck}. */
@@ -194,23 +197,76 @@ class IterCallableYieldTypesMatchDocTypedCheckTest {
   private IDefinitionKeeper withSimpleVectorForScatter() {
     final IDefinitionKeeper definitionKeeper = new DefinitionKeeper();
     definitionKeeper.add(
-        new nl.ramsolutions.sw.magik.analysis.definitions.MethodDefinition(
+        new MethodDefinition(
             null,
             null,
             null,
             null,
             null,
-            nl.ramsolutions.sw.magik.analysis.typing.TypeString.SW_SIMPLE_VECTOR,
+            TypeString.SW_SIMPLE_VECTOR,
             "for_scatter()",
-            java.util.EnumSet.noneOf(
-                nl.ramsolutions.sw.magik.analysis.definitions.MethodDefinition.Modifier.class),
+            java.util.EnumSet.noneOf(MethodDefinition.Modifier.class),
             java.util.Collections.emptyList(),
             null,
             null,
-            new nl.ramsolutions.sw.magik.analysis.typing.ExpressionResultString(
-                nl.ramsolutions.sw.magik.analysis.typing.TypeString.ofVariadic(
-                    nl.ramsolutions.sw.magik.analysis.typing.TypeString.ofGenericReference("E"))),
-            nl.ramsolutions.sw.magik.analysis.typing.ExpressionResultString.EMPTY));
+            new ExpressionResultString(TypeString.ofVariadic(TypeString.ofGenericReference("E"))),
+            ExpressionResultString.EMPTY));
     return definitionKeeper;
+  }
+
+  @Test
+  void testUntypedLoopDocAlongsideTypedLoopDoc() {
+    final String code =
+        """
+        _iter _method a.b()
+          ## Yields integers. The declared
+          ## @loop must survive reasoning.
+          ## @loop {integer}
+          _loopbody(1)
+        _endmethod
+        """;
+    final IDefinitionKeeper definitionKeeper = new DefinitionKeeper();
+    final MagikTypedCheck check = new IterCallableYieldTypesMatchDocTypedCheck();
+    assertThat(check).reportsIssueCount(code, definitionKeeper, 1);
+  }
+
+  @Test
+  void testUntypedLoopDocOnly() {
+    final String code =
+        """
+        _iter _method a.b()
+          ## @loop Description, but no type.
+          _loopbody(1)
+        _endmethod
+        """;
+    final IDefinitionKeeper definitionKeeper = new DefinitionKeeper();
+    final MagikTypedCheck check = new IterCallableYieldTypesMatchDocTypedCheck();
+    assertThat(check).reportsIssueCount(code, definitionKeeper, 1);
+  }
+
+  @Test
+  void testUntypedLoopDocOnNonIterMethod() {
+    final String code =
+        """
+        _method a.b()
+          ## @loop Description, but no type.
+        _endmethod
+        """;
+    final IDefinitionKeeper definitionKeeper = new DefinitionKeeper();
+    final MagikTypedCheck check = new IterCallableYieldTypesMatchDocTypedCheck();
+    assertThat(check).reportsIssueCount(code, definitionKeeper, 1);
+  }
+
+  @Test
+  void testUntypedLoopDocOnAbstractIterMethod() {
+    final String code =
+        """
+        _abstract _iter _method a.b()
+          ## @loop Description, but no type.
+        _endmethod
+        """;
+    final IDefinitionKeeper definitionKeeper = new DefinitionKeeper();
+    final MagikTypedCheck check = new IterCallableYieldTypesMatchDocTypedCheck();
+    assertThat(check).reportsIssueCount(code, definitionKeeper, 1);
   }
 }
